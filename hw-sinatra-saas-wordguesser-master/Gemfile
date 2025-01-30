aeb30c5b4fa504cf4d300ccfdfb8284de695079c
zhumengzhiren <york.chen@mail.utoronto.ca>
2025-01-30 16:54:14 +0800

    part1

--- hw-sinatra-saas-wordguesser-master/Gemfile
+++ hw-sinatra-saas-wordguesser-master/Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.6.6'
+ruby '2.6.10'
 
 gem 'sinatra', '>= 2.0.2'
 gem 'sinatra-flash', '0.3.0'
@@ -15,7 +15,7 @@ group :development, :test do
   gem 'rack-test', '0.6.3'
   gem 'rack_session_access', '0.1.1'
 
-  gem 'byebug', '5.0.0'
+  gem 'byebug', '11.0.0'
   gem 'launchy', '2.4.3'
   gem 'rerun', '0.10.0'
 
--- hw-sinatra-saas-wordguesser-master/lib/wordguesser_game.rb
+++ hw-sinatra-saas-wordguesser-master/lib/wordguesser_game.rb
@@ -1,4 +1,5 @@
 class WordGuesserGame
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
@@ -6,8 +7,60 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def guess(letter)
+    raise ArgumentError, 'Guess must be a single letter' unless valid_guess?(letter)
+    letter = letter.downcase 
+    # Check if the letter is in the word
+
+    # Check if the letter has already been guessed
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    if @word.include?(letter)
+      @guesses += letter unless @guesses.include?(letter)
+      true
+    else
+      @wrong_guesses += letter unless @wrong_guesses.include?(letter)
+      true
+    end
+  end
+
+  def valid_guess?(letter)
+    return false if letter.nil? || letter.empty? || letter !~ /[a-zA-Z]/
+    true
+  end
+
+  def guess_several_letters(game, letters)
+    letters.each_char { |letter| game.guess(letter) }
+  end
+
+  def word_with_guesses
+    displayed_word = ''
+    @word.each_char do |char|
+      if @guesses.include?(char)
+        displayed_word += char
+      else
+        displayed_word += '-'
+      end
+    end
+    displayed_word
+  end
+
+  def check_win_or_lose
+    if @word.split('').all? { |char| @guesses.include?(char) }
+      :win
+    elsif @wrong_guesses.size >= 7
+      :lose
+    else
+      :play
+    end
+  end
+  
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
@@ -21,3 +74,5 @@ class WordGuesserGame
     end
   end
 end
+
+
