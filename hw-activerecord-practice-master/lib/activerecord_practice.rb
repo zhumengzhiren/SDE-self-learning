23ea6977c84f7916cc04f782e9ad8d6ca90ec76f
zhumengzhiren <york.chen@mail.utoronto.ca>
2025-01-30 21:18:51 +0800

    4.3done

--- hw-activerecord-practice-master/lib/activerecord_practice.rb
+++ hw-activerecord-practice-master/lib/activerecord_practice.rb
@@ -23,10 +23,59 @@ class Customer < ApplicationRecord
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
     # probably something like:  Customer.where(....)
+    Customer.where(first: 'Candice')
   end
 
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where("email LIKE '%@%'")
   end
   # etc. - see README.md for more details
+
+  def self.with_dot_org_email
+    Customer.where("email LIKE ?", '%.org')
+  end
+
+  def self.with_invalid_email
+    Customer.where("email != '' AND email NOT LIKE '%@%'")
+  end
+
+  def self.with_blank_email
+    Customer.where("email IS NULL OR email = ''")
+  end
+
+  def self.born_before_1980
+    Customer.where("birthdate < ?", Date.new(1980, 1, 1))
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.where("email LIKE '%@%' AND birthdate < ?", Date.new(1980, 1, 1))
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE 'B%'").order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    Customer.order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    customer = Customer.find_by(first: 'Gussie', last: 'Murray')
+    customer.update(birthdate: Date.new(2004, 2, 8)) if customer
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    Customer.where("email NOT LIKE '%@%'").update_all(email: '')
+  end
+
+  def self.delete_meggie_herman
+    customer = Customer.find_by(first: 'Meggie', last: 'Herman')
+    customer.destroy if customer
+  end
+
+  def self.delete_everyone_born_before_1978
+    Customer.where('birthdate < ?', Date.new(1978, 1, 1)).destroy_all
+  end
 end
+
--- hw-activerecord-practice-master/spec/activerecord_practice_spec.rb
+++ hw-activerecord-practice-master/spec/activerecord_practice_spec.rb
@@ -38,41 +38,41 @@ describe 'ActiveRecord practice' do
         expect(Customer).to receive(:where).at_least(:once).and_call_original
       end
 
-      xspecify 'anyone with first name Candice' do
+      specify 'anyone with first name Candice' do
         check Customer.any_candice, [24]
       end
 
-      xspecify 'with valid email (email addr contains "@") (HINT: look up SQL LIKE operator)' do
+      specify 'with valid email (email addr contains "@") (HINT: look up SQL LIKE operator)' do
         check Customer.with_valid_email, [1, 2, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 23, 26, 29, 30]
       end
 
-      xspecify 'with .org emails' do
+      specify 'with .org emails' do
         check Customer.with_dot_org_email, [5, 7, 8, 12, 23, 26, 29]
       end
 
-      xspecify 'with invalid but nonblank email (does not contain "@")' do
+      specify 'with invalid but nonblank email (does not contain "@")' do
         check Customer.with_invalid_email, [3, 6, 9, 16, 22, 25, 27, 28]
       end
 
-      xspecify 'with blank email' do
+      specify 'with blank email' do
         check  Customer.with_blank_email, [21, 24]
       end
 
-      xspecify 'born before 1 Jan 1980' do
+      specify 'born before 1 Jan 1980' do
         check Customer.born_before_1980, [3, 8, 9, 11, 15, 16, 17, 19, 20, 24, 25, 27]
       end
 
-      xspecify 'with valid email AND born before 1/1/1980' do
+      specify 'with valid email AND born before 1/1/1980' do
         check Customer.with_valid_email_and_born_before_1980, [8, 11, 15, 17, 19, 20]
       end
 
-      xspecify 'with last names starting with "B", sorted by birthdate' do
+      specify 'with last names starting with "B", sorted by birthdate' do
         expect(Customer.last_names_starting_with_b.map(&:id)).to eq([25, 23, 4, 28, 18, 21, 29, 1])
       end
     end
 
     describe 'without needing .where' do
-      xspecify '20 youngest customers, in any order (hint: lookup ActiveRecord `order` and `limit`)' do
+      specify '20 youngest customers, in any order (hint: lookup ActiveRecord `order` and `limit`)' do
         check Customer.twenty_youngest, [7, 5, 6, 30, 1, 10, 29, 21, 18, 13, 14, 28, 26, 4, 2, 22, 23, 12, 11, 9]
       end
     end
@@ -83,22 +83,22 @@ describe 'ActiveRecord practice' do
       expect(Customer).not_to receive(:find)
     end
 
-    xspecify 'the birthdate of Gussie Murray to February 8,2004 (HINT: lookup `Time.parse`)' do
+    specify 'the birthdate of Gussie Murray to February 8,2004 (HINT: lookup `Time.parse`)' do
       Customer.update_gussie_murray_birthdate
       expect(Customer.find_by(first: 'Gussie').birthdate.to_date).to eq(Date.new(2004, 2, 8))
     end
 
-    xspecify 'all invalid emails to be blank' do
+    specify 'all invalid emails to be blank' do
       Customer.change_all_invalid_emails_to_blank
       expect(Customer.where("email != '' AND email IS NOT NULL and email NOT LIKE '%@%'").count).to be_zero
     end
 
-    xspecify 'database by deleting customer Meggie Herman' do
+    specify 'database by deleting customer Meggie Herman' do
       Customer.delete_meggie_herman
       expect(Customer.find_by(first: 'Meggie', last: 'Herman')).to be_nil
     end
 
-    xspecify 'database by deleting all customers born on or before 31 Dec 1977' do
+    specify 'database by deleting all customers born on or before 31 Dec 1977' do
       Customer.delete_everyone_born_before_1978
       expect(Customer.where('birthdate < ?', Time.zone.parse("1 January 1978"))).to be_empty
     end
